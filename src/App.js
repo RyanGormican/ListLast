8da6193cc6665830e9ece279b467520dbaca2640
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-19 22:54:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import {
   faCircleCheck, faPen, faTrashCan
 } from '@fortawesome/free-solid-svg-icons';
 import {nanoid} from 'nanoid';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 function App() {
   
-  const [taskList, settaskList] = useState( [{taskId: nanoid(), taskText:'update the list with your tasks!', taskDone: false}]);
+  const [taskList, setTaskList] = useState( [{taskId: nanoid(), taskText:'update the list with your tasks!', taskDone: false}]);
+   useEffect(() => {
+      const getTasks = JSON.parse(localStorage.getItem('listlast-data'));
+    if (getTasks){
+     setTaskList(getTasks);   
+    }
+  },[]);
+   useEffect(() => {
+      localStorage.setItem('listlast-data', JSON.stringify(taskList));
+  }, [taskList]);
+  
   return (
     <div className="container App">
     <br /><br />
