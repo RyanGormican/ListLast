bf492fd701ba8901d5746ce8984fae8599324668
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-19 22:46:14 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+imoprt { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/free-solid-svg-icons';
+import React, {useState} from 'react';
 function App() {
   return (
     <div className="App">
