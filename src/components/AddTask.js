0ff3ad8b81ac34b6565dd56100d64dfc3dab310c
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-20 03:10:37 -0600

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,6 +1,19 @@
 const AddTask = ({ })=> {
   return (
+      <>
+ // New Task
+    <div className="row">
+    <div className="col">
+    <input value={addTask} onChange={ (e) => setAddTask(e.target.value)}
     
+    className="form-control form-control-lg" />
+    </div>
+    <div className="col-auto">
+    <button onClick={newTask} className="btn btn-lg btn-success"> New Task</button>
+    </div>
+    </div>
+    <br />    
+  </>
     )
 }
 
