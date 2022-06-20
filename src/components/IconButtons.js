6e6cbc2145600a396c9c59629f0dd5af5b9d35be
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-20 03:40:28 -0600

    Update IconButtons.js

--- src/components/IconButtons.js
+++ src/components/IconButtons.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCircleCheck, faPen, faTrashCan
-} from '@fortawesome/free-solid-svg-icons';
+} from '@fortawesome/free-solid-svg-icons'
 const IconButtons = ({ taskList, taskDone, setUpdateTask, removeTask  })=> {
   return (
        {taskList && taskList.map( (task, index) => {
