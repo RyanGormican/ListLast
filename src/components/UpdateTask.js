85165a69821a46b3d024368297e4aa8010d575ff
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-20 03:13:27 -0600

    Update UpdateTask.js

--- src/components/UpdateTask.js
+++ src/components/UpdateTask.js
@@ -1,6 +1,24 @@
 const UpdateTask = ({ })=> {
   return (
-    
+      <>
+      <div className="row">
+    <div className="col">
+    <input 
+    value={ updateTask && updateTask.taskText }
+    onChange={ (e) => switchTask(e)}
+    className="form-control form-control-lg" />
+    </div>
+    <div className="col-auto">
+    <button 
+    onClick={changeTask}
+className="btn btn-lg btn-success"> Update Task</button>
+        <button 
+ onClick={clearTask}
+className="btn btn-lg btn-success"> Cancel</button>
+    </div>
+    </div>
+    <br />
+  </>
     )
 }
 
